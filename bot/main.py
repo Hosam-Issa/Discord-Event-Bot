cecf764d2e3142fd2c59299e296f6261e21e53e4
Hosam-Issa <issahosam23@gmail.com>
2026-09-20 18:58:20 -0400

    Addition of leaderboards cog to main.

--- bot/main.py
+++ bot/main.py
@@ -1,8 +1,6 @@
 import discord
 from discord.ext import commands
-from dotenv import load_dotenv
-import os
-from bot.db.database import get_connection, init_db
+from bot.db.database import init_db
 from bot.db.seed import seed_if_needed
 from bot.config import DISCORD_TOKEN, GUILD_ID
 from bot.cogs.approvals import ApprovalView
@@ -19,6 +17,7 @@ async def on_ready():
     await bot.load_extension("bot.cogs.submissions")
     await bot.load_extension("bot.cogs.teams")
     await bot.load_extension("bot.cogs.approvals")
+    await bot.load_extension("bot.cogs.leaderboards")
     bot.add_view(ApprovalView())
 
     bot.tree.copy_global_to(guild=discord.Object(id=GUILD_ID))
@@ -31,4 +30,4 @@ async def on_app_command_error(interaction: discord.Interaction, error: discord.
         await interaction.response.send_message("Something went wrong. Please try again later or contact an admin.", ephemeral=True)
     print(f"Command error: {error}")
 
-bot.run(os.getenv("DISCORD_TOKEN"))
+bot.run(DISCORD_TOKEN)
